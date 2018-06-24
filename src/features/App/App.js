511a5d82b6cb5d243e84404390093330e1487f0a
Artem Starotitorov <starotitorov1997@gmail.com>
2018-06-24 20:36:16 +0300

    Add github-corner

--- src/features/App/App.js
+++ src/features/App/App.js
@@ -1,58 +1,25 @@
 import React, { Component } from 'react';
-import styled, { keyframes } from 'styled-components';
+import GithubCorner from 'react-github-corner';
 import { Container, Row, Col } from 'reactstrap';
 import Posts  from '../Posts';
-import logo from './logo.svg';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Header>
-          <Logo src={logo} alt="logo" />
-          <Title>Welcome to React</Title>
-        </Header>
-        <main>
-          <Container>
-            <Row>
-              <Col sm={{ size: 6, offset: 3 }}>
-                <Posts />
-              </Col>
-            </Row>
-          </Container>
-        </main>
-      </div>
+      <main>
+        <Container>
+          <Row>
+            <Col sm={{ size: 6, offset: 3 }}>
+              <Posts />
+            </Col>
+          </Row>
+        </Container>
+        <GithubCorner href="https://github.com/Starotitorov/react-tutorial-app" />
+      </main>
     );
   }
 }
-
-const Header = styled.header`
-  background-color: #222;
-  height: 150px;
-  padding: 20px;
-  color: white;
-  text-align: center;
-`;
-
-const appLogoSpin = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-
-  to {
-    transform: rotate(360deg);
-  }
-`;
-
-const Logo = styled.img`
-  animation: ${appLogoSpin} infinite 20s linear;
-  height: 80px;
-`;
-
-const Title = styled.h1`
-  font-size: 1.5em;
-`;
 
 export default App;
