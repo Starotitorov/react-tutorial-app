12138ac1552b68704a1794102c6eb18bf18bb859
Artem Starotitorov <starotitorov1997@gmail.com>
2020-02-16 21:16:08 +0300

    Update layout

--- src/services/rest.js
+++ src/services/rest.js
@@ -4,11 +4,27 @@ import { API_URL } from '../config';
 const restService = {};
 
 export const fetchPosts = () =>
-  axios.get(`${API_URL}/posts`)
+  axios.get(
+    `${API_URL}/posts`,
+    {
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      }
+    }
+  )
     .then(response => response.data);
 
-export const addPost = post => axios.post(`${API_URL}/posts`, post)
-  .then(response => response.data);
+export const addPost = post =>
+  axios.post(
+    `${API_URL}/posts`,
+    post,
+    {
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8'
+      }
+    }
+  )
+    .then(response => response.data);
 
 restService.api = {
   fetchPosts,
